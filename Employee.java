59e39e2d79809c884a3b0e4fcdbd812a78cb1547
fatihhhyaaa <friskiramdhani@gmail.com>
2024-10-31 11:48:17 +0700

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    String name;
+    double salary;
+
+    public Employee(String namaEmployee, double GajiEmployee) {
+        name = namaEmployee;
+        salary = GajiEmployee;
+
+    }
+
+    public void print() {
+        System.out.println("nama " + name);
+        System.out.println("Gaji " + salary);     
+    }
+
+}
